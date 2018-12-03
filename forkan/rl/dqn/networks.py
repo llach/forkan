4acecf8604e1adc1698aa633f2e073ad11c93e44
llach <llach@techfak.uni-bielefeld.de>
2018-12-03 14:14:40 +0100

    DQN: added dueling networks to build_network()
    also factored out mlp creation in separate method

--- forkan/rl/dqn/networks.py
+++ forkan/rl/dqn/networks.py
@@ -2,6 +2,8 @@ import sys
 import logging
 import tensorflow as tf
 
+from forkan.common.tf_utils import scalar_summary
+
 """
 YETI - YEt To Implement
 
@@ -13,23 +15,49 @@ layer_norm: bool
 logger = logging.getLogger(__name__)
 
 
-def build_network(input_shape, num_actions, network_type='mlp', scope='', reuse=None):
+def _mlp(input_, num_outputs, hiddens=[], output_activation=None):
+
+    out = input_
+    for h in hiddens:
+        out = tf.contrib.layers.fully_connected(out, h, activation_fn=None)
+        out = tf.nn.relu(out)
+    return tf.contrib.layers.fully_connected(out, num_outputs, activation_fn=output_activation)
+
+
+def build_network(input_shape, num_actions, dueling=False, network_type='mini-mlp', scope='', reuse=None,
+                  summaries=False):
 
     # use variable scope given and reuse is necessary
     with tf.variable_scope(scope, reuse=reuse):
 
-        if network_type is 'mlp':
+        if network_type == 'mini-mlp':
             # add one dimension to shape
             input_shape = (None,) + input_shape
 
             # build standard MLP
-            _input = tf.placeholder(tf.float32, shape=input_shape, name='input')
-            dense1 = tf.contrib.layers.fully_connected(_input, 24)
-            dense2 = tf.contrib.layers.fully_connected(dense1, 24)
-            output = tf.contrib.layers.fully_connected(dense2, num_actions, activation_fn=None)
+            input_ = tf.placeholder(tf.float32, shape=input_shape, name='input')
+
+            if not dueling:
+                output = _mlp(input_, num_actions, [24, 24])
+            else:
+                # advantage value function
+                A = _mlp(input_, num_actions, [24, 24])
+
+                # state-value function
+                V = _mlp(input_, num_actions, [24, 24])
+
+                # plot mean(V(s, a))
+                if summaries:
+                    scalar_summary('state_value', tf.reduce_mean(V))
+
+                # mean-center advantage values
+                A_mean = tf.reduce_mean(A, axis=1)
+                A_centered = A - tf.expand_dims(A_mean, axis=1)
+
+                output = V + A_centered
 
         else:
             logger.critical('Network type {} unknown!'.format(network_type))
             sys.exit(0)
 
-    return output, _input
+    return output, input_
