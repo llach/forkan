64efd68310dd5c515ca85e6458a81fc050d7026d
llach <llach@techfak.uni-bielefeld.de>
2018-12-03 10:48:34 +0100

    DQN: added reuse option to network creation

--- forkan/rl/dqn/networks.py
+++ forkan/rl/dqn/networks.py
@@ -13,21 +13,23 @@ layer_norm: bool
 logger = logging.getLogger(__name__)
 
 
-def build_network(input_shape, num_actions, network_type='mlp', name=''):
+def build_network(input_shape, num_actions, network_type='mlp', scope='', reuse=False):
 
-    if network_type is 'mlp':
-        # add one dimension to shape
-        input_shape = (None,) + input_shape
+    # use variable scope given and reuse is necessary
+    with tf.variable_scope(scope, reuse=reuse):
 
-        with tf.variable_scope(name):
+        if network_type is 'mlp':
+            # add one dimension to shape
+            input_shape = (None,) + input_shape
+
             # build standard MLP
             _input = tf.placeholder(tf.float32, shape=input_shape, name='input')
             dense1 = tf.contrib.layers.fully_connected(_input, 24)
             dense2 = tf.contrib.layers.fully_connected(dense1, 24)
             output = tf.contrib.layers.fully_connected(dense2, num_actions, activation_fn=None)
 
-    else:
-        logger.critical('Network type {} unknown!'.format(network_type))
-        sys.exit(0)
+        else:
+            logger.critical('Network type {} unknown!'.format(network_type))
+            sys.exit(0)
 
     return output, _input
