6baa4dd8f0514f1f5a1380a31ebb1723bb2dc825
llach <llach@techfak.uni-bielefeld.de>
2019-02-12 16:54:00 +0100

    setup.py: added gpu option for tensorflow

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
+import argparse
+
 from setuptools import setup
 from setuptools import find_packages
+
+# parse args
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', '-g', action='store_true')
+args, unknown = parser.parse_known_args()
+
+tf_suffix = '-gpu' if args.gpu else ''
 
 setup(name='forkan',
       version='0.0.1',
@@ -7,7 +16,7 @@ setup(name='forkan',
       author='Luca Lach',
       author_email='llach@teachfak.uni-bielefeld.de',
       url='https://github.com/llach/dl-toolkit',
-      install_requires=['tensorflow-gpu>=1.11.0',
+      install_requires=['tensorflow{}>=1.11.0'.format(tf_suffix),
                         'numpy>=1.15.3',
                         'tabulate>=0.8.2',
                         'Pillow>=5.3.0',
