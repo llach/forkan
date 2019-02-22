7910d21c48ac721cb802446e022af3f97d9c336d
llach <llach@techfak.uni-bielefeld.de>
2019-02-22 22:31:49 +0100

    datasets: implemented loading atari datasets

--- forkan/datasets/__init__.py
+++ forkan/datasets/__init__.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+from forkan import dataset_path
+
 from forkan.datasets.mnist import load_mnist
 from forkan.datasets.image import load_unlabeled_image_dataset
 from forkan.datasets.dsprites import load_dsprites, load_dsprites_one_fixed
+
+def load_atari_normalized(env):
+    name = env.replace('NoFrameskip', '').lower().split('-')[0]
+    return np.load('{}/{}-normalized.npz'.format(dataset_path, name))['data']
