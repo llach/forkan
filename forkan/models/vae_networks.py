7e09cf2dce50b78758eaf789f0ffa52eaf3c576e
llach <llach@techfak.uni-bielefeld.de>
2019-02-22 09:33:48 +0100

    vae-networks: using logger instead of print

--- forkan/models/vae_networks.py
+++ forkan/models/vae_networks.py
@@ -1,6 +1,10 @@
 import numpy as np
 import tensorflow as tf
 
+import logging
+
+log = logging.getLogger('vae-nets')
+
 
 def _build_conv(x, x_shape, rec_shape, latent_dim, network_type,
                 encoder_conf, decoder_conf, hiddens):
@@ -8,46 +12,46 @@ def _build_conv(x, x_shape, rec_shape, latent_dim, network_type,
     num_channels = x.shape[-1]
 
     with tf.name_scope('encoder'):
-        print('\n===== VAE-{} ===== '.format(network_type))
-        print('input: {}'.format(x.shape))
-        print('\n===== encoder')
+        log.info('===== {}-network ===== '.format(network_type))
+        log.info('input: {}'.format(x.shape))
+        log.info('===== encoder')
         for n, (filters, kernel_size, stride) in enumerate(encoder_conf):
             x = tf.contrib.layers.conv2d(inputs=x,
                                          num_outputs=filters,
                                          kernel_size=kernel_size,
                                          stride=stride,
                                          activation_fn=tf.nn.relu)
-            print('conv {} => {}'.format(n, x.shape))
+            log.info('conv {} => {}'.format(n, x.shape))
 
         encoder_last_conv_shape = x.shape
 
         flat_encoder = tf.layers.flatten(x)
-        print('flatten => {}'.format(flat_encoder.shape))
+        log.info('flatten => {}'.format(flat_encoder.shape))
 
         fc = tf.contrib.layers.fully_connected(flat_encoder, hiddens, activation_fn=tf.nn.relu)
-        print('fc [ReLu] => {}'.format(fc.shape))
-        print('\n===== latent-{}'.format(latent_dim
+        log.info('fc [ReLu] => {}'.format(fc.shape))
+        log.info('===== latent-{}'.format(latent_dim
                                          ))
         mus = tf.contrib.layers.fully_connected(fc, latent_dim, activation_fn=None)
         logvars = tf.contrib.layers.fully_connected(fc, latent_dim, activation_fn=None)
-        print('mus: {}'.format(mus.shape))
-        print('logvars: {}'.format(logvars.shape))
+        log.info('mus: {}'.format(mus.shape))
+        log.info('logvars: {}'.format(logvars.shape))
 
         z = mus + tf.exp(0.5 * logvars) * tf.random_normal(tf.shape(mus))
 
-        print('==> z {}'.format(z.shape))
-        print('\n===== encoder')
+        log.info('==> z {}'.format(z.shape))
+        log.info('===== encoder')
 
     with tf.name_scope('decoder'):
         fcd = tf.contrib.layers.fully_connected(z, hiddens, activation_fn=tf.nn.relu)
-        print('fc [ReLu] => {}'.format(fcd.shape))
+        log.info('fc [ReLu] => {}'.format(fcd.shape))
 
         fcd2 = tf.contrib.layers.fully_connected(z, int(np.prod(encoder_last_conv_shape[1:])), activation_fn=None)
-        print('fc [None] => {}'.format(fcd2.shape))
+        log.info('fc [None] => {}'.format(fcd2.shape))
 
         x = tf.reshape(fcd2, shape=rec_shape)
 
-        print('reshape => {}'.format(x.shape))
+        log.info('reshape => {}'.format(x.shape))
 
         for n, (filters, kernel_size, stride) in enumerate(decoder_conf):
             x = tf.contrib.layers.conv2d_transpose(inputs=x,
@@ -55,7 +59,7 @@ def _build_conv(x, x_shape, rec_shape, latent_dim, network_type,
                                                    kernel_size=kernel_size,
                                                    stride=stride,
                                                    activation_fn=tf.nn.relu)
-            print('conv.T {} => {}'.format(n, x.shape))
+            log.info('conv.T {} => {}'.format(n, x.shape))
 
         out = tf.contrib.layers.conv2d_transpose(inputs=x,
                                                  num_outputs=num_channels,
@@ -63,8 +67,7 @@ def _build_conv(x, x_shape, rec_shape, latent_dim, network_type,
                                                  stride=1,
                                                  activation_fn=tf.nn.sigmoid)
 
-    print('output: {}'.format(out.shape))
-
+    log.info('output: {}'.format(out.shape))
     assert x_shape[1:] == out.shape[1:], 'input\'s {} and ouput\'s {} shape need to match!'.format(x_shape[1:],
                                                                                                    out.shape[1:])
 
@@ -72,7 +75,6 @@ def _build_conv(x, x_shape, rec_shape, latent_dim, network_type,
 
 
 def build_network(x, x_shape, latent_dim=10, network_type='atari'):
-    print(network_type)
 
     if network_type == 'atari':
         # this must not depend on input tensor, otherwise the decoder graph
@@ -103,7 +105,7 @@ def build_network(x, x_shape, latent_dim=10, network_type='atari'):
 
         hiddens = 256
     else:
-        print('network \'{}\' unknown'.format(network_type))
+        log.cirical('network \'{}\' unknown'.format(network_type))
         exit(1)
 
     return _build_conv(x, x_shape, rec_shape, latent_dim, network_type, encoder_conf, decoder_conf, hiddens)
