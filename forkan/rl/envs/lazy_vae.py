d60f9773bcf4d41f64e59b5eef97a965c30edbd0
llach <llach@techfak.uni-bielefeld.de>
2019-02-24 13:50:31 +0100

    lazy-vae-env: processing obs; adjusted observation space

--- forkan/rl/envs/lazy_vae.py
+++ forkan/rl/envs/lazy_vae.py
@@ -1,18 +1,10 @@
 import logging
+import numpy as np
 import tensorflow as tf
 
-from gym import Space
+from gym import spaces
 from forkan.rl import EnvWrapper
 from forkan.models import VAE
-
-
-"""
-TODO
-
-- normalize
-- batch-pass that mofo to vae
-- pass session to restore vae graph in
-"""
 
 
 class LazyVAE(EnvWrapper):
@@ -37,14 +29,25 @@ class LazyVAE(EnvWrapper):
         # inheriting from EnvWrapper and passing it an env makes spaces available.
         super().__init__(env)
 
-        # separate session for VAE graph
+        # separate session for VAE graph & create VAE
         self.vae_s = tf.Session()
-
         self.vae = VAE(load_from=load_from, sess=self.vae_s)
+
+        bs = self.observation_space.shape[-1]
+        self.observation_space = spaces.Box(low=0.0, high=1.0, shape=(bs*2*self.vae.latent_dim,))
+
+    def _process(self, obs):
+        # normalize obs
+        obs = np.asarray(obs) / 255
+
+        # channels -> batch
+        obs = np.expand_dims(np.moveaxis(obs, -1, 0), -1)
+
+        return self.vae.encode(obs).flatten()
 
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
-        return obs, reward, done, info
+        return self._process(obs), reward, done, info
 
     def reset(self):
-        return self.env.reset()
+        return self._process(self.env.reset())
