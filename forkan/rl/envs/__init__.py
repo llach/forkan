dd0d54c715390f1447b9182f946c1dac1a66de47
llach <llach@techfak.uni-bielefeld.de>
2019-03-07 15:06:44 +0100

    rl.envs: new pendulumenv

--- forkan/rl/envs/__init__.py
+++ forkan/rl/envs/__init__.py
@@ -3,3 +3,4 @@ from .multi_env import MultiEnv
 from .multi_stepper import MultiStepper
 from .lazy_vae import LazyVAE
 from .fake_lazy_vae import FakeLazyVAE
+from .pendulum_render import PendulumRenderEnv
---
+++ forkan/rl/envs/pendulum_render.py
@@ -0,0 +1,57 @@
+import logging
+import numpy as np
+from skimage.transform import resize
+
+from gym import spaces
+from forkan.rl import EnvWrapper
+
+
+class PendulumRenderEnv(EnvWrapper):
+
+    def __init__(self,
+                 env,
+                 **kwargs,
+                 ):
+        """
+        Wraps Pendulum environment, renders it and returns greyscaled and cropped images as obs.
+
+
+        WARNING this also emulates a VecEnv for A2C with one environment
+
+        env: gym.Environment
+            FrameStackEnv that buffers LazyFrames
+
+        load_from: string
+            argument passed to VAE: location of weights
+        """
+
+        self.logger = logging.getLogger(__name__)
+
+        # inheriting from EnvWrapper and passing it an env makes spaces available.
+        super().__init__(env)
+
+        self.num_envs = 1
+
+        self.observation_space = spaces.Box(low=0.0, high=1.0, shape=(64, 64, 1))
+
+    def _process(self, obs):
+        obs = obs / 255  # normalise
+        obs = obs[121:256 + 121, 121:256 + 121, :]  # cut out interesting area
+        obs = np.dot(obs[..., :3], [0.299, 0.587, 0.114])  # inverted greyscale
+        obs = resize(obs, (64, 64))
+        return np.expand_dims(np.expand_dims(obs, -1), 0)
+
+    def step(self, action):
+        _, reward, done, info = self.env.step(action)
+
+        if done:
+            obs = self.reset()
+            return obs, reward, [done], info
+        else:
+            obs = self.env.render(mode='rgb_array')
+            return self._process(obs), reward, [done], info
+
+    def reset(self):
+        _ = self.env.reset()
+        obs = self.env.render(mode='rgb_array')
+        return self._process(obs)
