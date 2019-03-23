5b4b9706f6bba0f6ce540519e2e92b47204a519d
llach <llach@techfak.uni-bielefeld.de>
2019-03-23 15:46:02 +0100

    trpo-vae-pend: testing entangled VAE

--- forkan/rl/envs/vae_stack.py
+++ forkan/rl/envs/vae_stack.py
@@ -13,6 +13,7 @@ class VAEStack(EnvWrapper):
 
     def __init__(self,
                  env,
+                 load_from,
                  k=3,
                  vae_network='pendulum',
                  **kwargs,
@@ -23,31 +24,24 @@ class VAEStack(EnvWrapper):
         # inheriting from EnvWrapper and passing it an env makes spaces available.
         super().__init__(env)
 
-        load_from = 'pendvisualuniform-b77.5-lat5-lr0.001-2019-03-21T00/13'.replace('/', ':')
-        self.idxes = [3, 4]
-
         self.k = k
         self.v = VAE(load_from=load_from, network=vae_network)
-        self.observation_space = spaces.Box(low=-np.infty, high=np.infty, shape=(2*self.k,),
+        self.observation_space = spaces.Box(low=-np.infty, high=np.infty, shape=(self.v.latent_dim*self.k,),
                                             dtype=np.float)
-
-        self.logger.warning('THIS VERSION IS USING HARDCODED INDICES, BE AWARE!')
-
         self.vae_name = self.v.savename
-        self.q = deque(maxlen=2*self.k)
+        self.q = deque(maxlen=self.k)
         self._reset_queue()
 
     def _reset_queue(self):
-        for _ in range(2*self.k):
-            self.q.appendleft(0)
+        for _ in range(self.k):
+            self.q.appendleft([0]*self.v.latent_dim)
 
     def _process(self, obs):
         mus, _, _ = self.v.encode(np.expand_dims(obs, 0))
-        self.q.appendleft(mus[0][self.idxes[0]])
-        self.q.appendleft(mus[0][self.idxes[1]])
+        self.q.appendleft(np.squeeze(mus))
 
     def _get_obs(self):
-        return np.asarray(self.q)
+        return np.asarray(self.q).flatten()
 
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
