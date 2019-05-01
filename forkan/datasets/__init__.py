27f65b58d1f0f95ad53d6d313eef60e09338c158
llach <llach@techfak.uni-bielefeld.de>
2019-05-01 08:56:24 +0200

    some new plots and evals

--- forkan/datasets/__init__.py
+++ forkan/datasets/__init__.py
@@ -1,17 +1,23 @@
 import numpy as np
 
 from forkan import dataset_path
-
-from forkan.datasets.mnist import load_mnist
-from forkan.datasets.image import load_unlabeled_image_dataset
 from forkan.datasets.dsprites import load_dsprites, load_dsprites_one_fixed
+from forkan.datasets.image import load_unlabeled_image_dataset
+from forkan.datasets.mnist import load_mnist
+
+
+def load_set(name):
+    return np.load('{}/{}.npz'.format(dataset_path, name))['data']
+
 
 def load_atari_normalized(env):
     name = env.replace('NoFrameskip', '').lower().split('-')[0]
     return np.load('{}/{}-normalized.npz'.format(dataset_path, name))['data']
 
+
 def load_pendulum():
     return np.load('{}/pendulum-visual-random-normalized-cut.npz'.format(dataset_path))['data']
+
 
 def load_uniform_pendulum():
     return np.load('{}/pendulum-visual-uniform.npz'.format(dataset_path))['data']
